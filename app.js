afd145e31ec1058c64615b5a46c9519d26b5aadc
CosmicTiger <luisangelmarcia@gmail.com>
2021-02-12 00:35:18 -0600

    :monocle_faces: - @CosmicTiger - Understanding CORS errors and how to interpret them

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form></form>
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // the domains can be specified like: 'codepen.io, etc.io'
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/app', todosRoutes);
 
 app.listen(8080);
