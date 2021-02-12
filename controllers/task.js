b1cb260adbd03b6698a4fc454de4cc3b1ed1d2e7
CosmicTiger <luisangelmarcia@gmail.com>
2021-02-12 01:26:19 -0600

    :construction: - @CosmicTiger - Done creating post endpoint

--- controllers/task.js
+++ controllers/task.js
@@ -10,7 +10,7 @@ exports.getTasks = (req, res, next) => {
             createdAt: date,
             getDoneAt: date,
             deadline: date.getHours() + ':' + date.getMinutes(),
-            label: ['Usual'],
+            labels: ['Usual'],
             priority: 1,
             isReminded: {
                 verification: false,
@@ -25,15 +25,31 @@ exports.getTasks = (req, res, next) => {
 
 exports.createTasks = (req, res, next) => {
     const title = req.body.title;
-    const content = req.body.content;
-    
+    const content = req.body.content; // This is comments
+    const inbox = req.body.inbox;
+    const getDoneAt = req.body.getDoneAt;
+    const deadline = req.body.deadline;
+    const labels = req.body.labels;
+    const priority = req.body.priority;
+    const isReminded = req.body.isReminded;
+    const user = req.body.user;
+    let date = new Date();
+
     // Create post method in db
     res.status(201).json({
         message: 'Post created successfully!',
-        post: {
-            id: new Date().toISOString(),
+        task: {
+            _id: new Date().toISOString(),
             title: title,
-            content: content
+            comments: content,
+            inbox: inbox,
+            createdAt: date,
+            getDoneAt: date,
+            deadline: deadline,
+            labels: labels,
+            priority: priority,
+            isReminded: isReminded,
+            user: user
         }
     });
 };
